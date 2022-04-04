d2009f1efc01591128f49324095698e63b28a448
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-04-04 16:06:24 +0100

    Final changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,13 @@ function App() {
                   <div className="card-title">
                     { giftcard.title }
                   </div>
-                  <div className="card-body">{ giftcard.title } gift-card can be converted into { giftcard.currencies.join(", ").toUpperCase() } currencies. </div>
+                  {/* <div className="card-body">{ giftcard.title } gift-card can be converted into { giftcard.currencies.join(", ").toUpperCase() } currencies. </div> */}
+                  <div className="card-body">{ giftcard.title } gift-card can be converted into the listed currencies </div>
+                  <div className='currency'>
+                    {giftcard.currencies.map( currency =>
+                      <p key={currency} className="individual-currency">{currency}</p>
+                    )}
+                  </div>
                 </div>
               )
             })
